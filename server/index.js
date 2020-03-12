20f68f19b501dbb407e6cf5954f4eeff15f9c65e
Bradley Zazzara <bradleyzazz@gmail.com>
2020-03-11 17:48:07 -0700

    Initial folder setup and configuration

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('../database/models');
+const port = 3000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlendcoded({extended: true}));
+
+app.use(express.static(__dirname + '/../client'));
+
+app.get('/api/reservations', (req, res) => {
+
+});X
