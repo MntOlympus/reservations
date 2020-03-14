365b3d36bb61b97db0a34169250bc22d92e9de3f
Bradley Zazzara <bradleyzazz@gmail.com>
2020-03-13 17:08:55 -0700

    Setup server and getProperty GET request

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const Controller = require('../server/models');
+const Controller = require('../server/controllers/getProperty.js');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlendcoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(__dirname + '/../client'));
 
-app.get('/api/reservations', (req, res) => {
+app.listen(port, () => console.log('server is listening on port 3000...'))
 
+
+app.get('/api/properties', (req, res) => {
+  Controller.getProperty(req, res);
 });
+
