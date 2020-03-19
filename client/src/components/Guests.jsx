3ea11bb7d2956e706a57341111455ac266989c4c
Bradley Zazzara <bradleyzazz@gmail.com>
2020-03-19 11:46:32 -0700

    Add temp files

---
+++ client/src/components/Guests.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+class Guests extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      totalGuests: 1,
+      adults: 1,
+      children: 0,
+      infantS: 0
+    }
+  }
+
+
+  render() {
+
+    return (
+
+    )
+  }
+}
